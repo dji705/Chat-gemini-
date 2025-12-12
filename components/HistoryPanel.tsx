b906002349702d399d8eedaa9706aca0dae7aced
@נתנאל_דרוטמן <7637771@gmail.com>
2025-12-12 14:53:28 +0200

    Remove HistoryPanel component

--- components/HistoryPanel.tsx
+++ components/HistoryPanel.tsx
@@ -1,12 +1,1 @@
-// src/components/HistoryPanel.tsx
-import React from 'react';
 
-const HistoryPanel = () => {
-  return (
-    <div>
-      <h2>History Panel</h2>
-    </div>
-  );
-};
-
-export default HistoryPanel;
